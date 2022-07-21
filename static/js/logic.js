65975648c6df075d95190d2bd802f65b31d72898
Reese Coulter <40220871+ActuallyReese@users.noreply.github.com>
2022-07-21 15:38:55 -0700

    Added pages and navigation

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,22 +1,15 @@
-// Add console.log to check to see if our code is working.
+// Add console.log to check to see if the code is working.
 console.log("working");
 
 function json(url) {
     return fetch(url).then(res => res.json());
   }
   
-  
+  let locations = document.querySelector("h2");
   let apiKey = API_KEY;
   json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-    console.log(data.ip);
+    // console.log(data.ip);
     console.log(data.city);
-    alert(data.ip + ", " + data.city + ", " + data.region);
+    locations.innerText = (data.city + ", " + data.region);
 
-   
 });
-
-
-function showLocation() {
-    json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-    alert(data.ip + ", " + data.city + ", " + data.region);
-})};
